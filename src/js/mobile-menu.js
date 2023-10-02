a60101cf371a01410fb77901c79c68a0c98bc4f5
Shadioso <114300237+Shadioso@users.noreply.github.com>
2023-10-02 23:11:08 +0300

    debug

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,14 +1,25 @@
 (() => {
   const menuBtnRef = document.querySelector('[data-menu-button]');
   const mobileMenuRef = document.querySelector('[data-menu]');
-
-  menuBtnRef.addEventListener('click', () => {
+  const body = document.querySelector(`body`);
+  //
+  const openMenu = () => {
     const expanded =
       menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
 
     menuBtnRef.classList.toggle('is-open');
     menuBtnRef.setAttribute('aria-expanded', !expanded);
-
+    body.classList.toggle(`disable-scroll`);
     mobileMenuRef.classList.toggle('is-open');
-  });
+  };
+  //
+  const changeDir = evt => {
+    if (evt.target.nodeName !== 'A') {
+      return;
+    } else if (mobileMenuRef.classList.contains(`is-open`)) {
+      openMenu();
+    }
+  };
+  mobileMenuRef.addEventListener(`click`, changeDir);
+  menuBtnRef.addEventListener('click', openMenu);
 })();
